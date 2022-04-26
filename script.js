88b1b3b6782d2300ca0b07dff28d28ce48c8a4f2
Sara Romão <sara-romao@live.com>
2022-04-26 17:43:12 -0300

    Estado busca

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const city = document.querySelector('#search');
+const state = document.getElementById("estado");
 
 const  btnsearch = document.querySelector('.btnsearch')
 
@@ -7,15 +8,24 @@ const  btnsearch = document.querySelector('.btnsearch')
 
 
 function teste(){
+    const citySearch= city.value.replace(" ", "%20")
+    const stateSearch=  state.value.replace(" ", "%20")
     
-    console.log(city.value.replace(" ", "%20"))
-}
+    console.log(citySearch, "estado: ", stateSearch)
 
-
-/* async function getApi(){
-    const response = await fetch('https://weather.contrateumdev.com.br/api/weather/city/?city=presidente%20prudente,são%20paulo');
+    async function getApi(){
+    const response = await fetch(`https://weather.contrateumdev.com.br/api/weather/city/?city=${citySearch},${stateSearch}`);
     const data = await response.json()
+
+    if(data){
+        
+    }
     console.log(data)
 }
 
-getApi() */
+ getApi()
+
+}
+
+
+
