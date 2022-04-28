1d35681eb2272e0266d492c3221f8db7dde983b7
Sara Romão <sara-romao@live.com>
2022-04-27 22:50:11 -0300

    estados api

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
-
-const city = document.querySelector('#search');
+const city = document.querySelector("#search");
 const state = document.getElementById("estado");
+const estadoArr = [];
 
-const  btnsearch = document.querySelector('.btnsearch')
+const btnsearch = document.querySelector(".btnsearch");
 
 
-
-
-function teste(){
-    const citySearch= city.value.replace(" ", "%20")
-    const stateSearch=  state.value.replace(" ", "%20")
+ async function getStates(){
+    const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome');
+    const dados = await response.json();
     
-    console.log(citySearch, "estado: ", stateSearch)
+   
+    state.innerHTML=`${dados.map((item)=> `<option value=${item.nome}>${item.nome}</option>`)}   `
+   /*  console.table(dados); 
+     */
 
-    async function getApi(){
-    const response = await fetch(`https://weather.contrateumdev.com.br/api/weather/city/?city=${citySearch},${stateSearch}`);
-    const data = await response.json()
+ }
 
-    if(data){
+ getStates()
+
+
+ function idEstado(name){
+    async function findEstado(){
+
+    
+        const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+        const dados = await response.json();
         
+        const id = dados.find((item) => item.nome === name)
+        console.log(id)
+   
     }
-    console.log(data)
+
+    findEstado()
+ }
+
+function cidades(){
+    const stateSearch = state.value;
+    idEstado(stateSearch)
+    console.log(stateSearch, "nome estado")
+  /*   async function cidades(){
+        const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios`)
+        const data = await response.json();
+
+    } */
 }
 
- getApi()
 
+
+
+/* function teste() {
+    const citySearch = city.value.replace(" ", "%20");
+    const stateSearch = state.value.replace(" ", "%20");
+
+    console.log(citySearch, "estado: ", stateSearch);
+
+    if(citySearch !== "" && stateSearch !== ""){
+        getApi();
+    }
+    async function getApi() {
+        const response = await fetch(
+            `https://weather.contrateumdev.com.br/api/weather/city/?city=${citySearch},${stateSearch}`
+        );
+        const data = await response.json();
+
+        if (data) {
+        }
+        console.log(data);
+    }
+
+    
 }
-
-
-
+ */
